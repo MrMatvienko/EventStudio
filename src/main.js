be3c40eb7a7b7a09eeaca229ee0e0b2821d081ab
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2024-11-10 17:54:54 +0200

    question section

--- src/main.js
+++ src/main.js
@@ -12,3 +12,36 @@ menuLinks.forEach((link) => {
     mobMenu.classList.add("is-hidden");
   });
 });
+
+/*--------------Question--Button--Arrow-------------------*/
+const arrowBtns = document.querySelectorAll(".question-icon");
+const answers = document.querySelectorAll(".question-answer");
+
+arrowBtns.forEach((arrow) => {
+  arrow.addEventListener("click", function () {
+    const questionItem = arrow.closest(".question-list-item");
+    const answer = questionItem.querySelector(".question-answer");
+
+    /*-----Closed all answer---------*/
+    answers.forEach((item) => {
+      if (item !== answer) {
+        item.classList.remove("active");
+        item.style.maxHeight = null;
+        const arrowIcon = item
+          .closest(".question-list-item")
+          .querySelector(".question-icon");
+        arrowIcon.style.transform = "rotate(0deg)";
+      }
+    });
+
+    answer.classList.toggle("active");
+
+    if (answer.classList.contains("active")) {
+      answer.style.maxHeight = answer.scrollHeight + "px";
+      arrow.style.transform = "rotate(180deg)";
+    } else {
+      answer.style.maxHeight = null;
+      arrow.style.transform = "rotate(0deg)";
+    }
+  });
+});
