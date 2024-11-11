ba68c2109d34a63996d9ebea7b0550e1dc291692
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2024-11-11 19:21:26 +0200

    swiper gallery

---
+++ src/JS/swiper.js
@@ -0,0 +1,28 @@
+import Swiper from "swiper";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+
+const swiper = new Swiper(".swiper", {
+  modules: [Pagination],
+  slidesPerView: 2,
+  slidesPerGroup: 2,
+  spaceBetween: 8,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    type: "bullets",
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1440: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+    },
+  },
+});
