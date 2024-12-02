f6c5c5ca18ed8ef09a46cf5aa876bd85862593da
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2024-12-02 14:41:29 +0200

    test fom add

--- src/JS/sendFormTest.js
+++ src/JS/sendFormTest.js
@@ -7,10 +7,23 @@ export function sendTest() {
     testForm.addEventListener("submit", function (event) {
       event.preventDefault();
 
+      const questions = {};
+      const fieldsets = document.querySelectorAll("fieldset");
+      fieldsets.forEach((fieldset, index) => {
+        const questionNumber = `question-${index + 1}`;
+        const checkboxes = fieldset.querySelectorAll(
+          `input[name="${questionNumber}"]:checked`
+        );
+        questions[questionNumber] = Array.from(checkboxes)
+          .map((checkbox) => checkbox.nextElementSibling.textContent.trim())
+          .join(", ");
+      });
+
       const params = {
         from_name: document.getElementById("form_name").value,
         phone_number: document.getElementById("phone").value,
         telegram: document.getElementById("telegram").value,
+        ...questions,
       };
 
       emailjs
