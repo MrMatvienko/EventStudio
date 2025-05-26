06dad68579e6b69b471454f40ca441b24a0da739
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2025-05-26 12:39:05 +0300

    up

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,26 @@ export default defineConfig({
   root: "src",
   build: {
     rollupOptions: {
-      input: globSync("./src/*.html"),
+      input: globSync("./src/**/index.html"), // ✅ читає всі сторінки як /about/, /contact/
     },
     outDir: "../dist",
   },
-  plugins: [injectHTML(), FullReload(["./src/**/**.html"])],
+  plugins: [injectHTML(), FullReload(["./src/**/*.html"])],
 });
+
+// import { defineConfig } from "vite";
+// import { sync as globSync } from "glob";
+// import injectHTML from "vite-plugin-html-inject";
+// import FullReload from "vite-plugin-full-reload";
+
+// export default defineConfig({
+//   base: "/EventStudio/",
+//   root: "src",
+//   build: {
+//     rollupOptions: {
+//       input: globSync("./src/*.html"),
+//     },
+//     outDir: "../dist",
+//   },
+//   plugins: [injectHTML(), FullReload(["./src/**/**.html"])],
+// });
